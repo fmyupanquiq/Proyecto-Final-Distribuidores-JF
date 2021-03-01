141f6050a94520faa30351b0c8f758e1efa476de
fmyupanquiq <78872019+fmyupanquiq@users.noreply.github.com>
2021-02-28 20:41:15 -0500

    Agregacion de la clase Producto
    En esta modificacion se agrega la codificacion de la clase Producto.

--- Producto.h
+++ Producto.h
@@ -1,12 +1,47 @@
-#ifndef PRODUCTO_H
-#define PRODUCTO_H
-//hcomentario
-class Producto
-{
+#include <iostream>
+#include<conio.h>
+#include <iomanip> 
+using namespace std;
+using std::cout; using std::endl;
+using std::string; 
+class Producto{
 	public:
-		Producto();
-	protected:
+		string  Nombre;
+		int Stock;
+		float Precio;
+		int Codigo;
+		string Descripcion;	
+	public:
+		void MostrarProducto();
+		void ActualizarCantidad(int );
+		int GetStock();
+		float GetPrecio();
+		string GetNombre();
+		int GetCod();		
+	
 };
-
-#endif
-
+void Producto::MostrarProducto(){
+	cout<<left;
+	cout<<setw(8)<<Codigo;
+	cout<<setw(10)<<Stock;
+	cout<<setw(22)<<Nombre;
+	cout<<setw(7)<<Precio;
+	cout<<setw(20)<<Descripcion;
+	cout<<endl;
+}
+void Producto::ActualizarCantidad(int k){
+	Stock=Stock-k;
+	
+}
+int Producto::GetStock(){
+	return Stock;
+}
+float Producto::GetPrecio(){
+	return Precio;
+}
+string Producto::GetNombre(){
+	return Nombre;
+}
+int Producto::GetCod(){
+	return Codigo;
+}
