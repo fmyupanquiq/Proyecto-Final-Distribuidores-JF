98e220fe78a10217190eea6d898d848bee788a02
fmyupanquiq <78872019+fmyupanquiq@users.noreply.github.com>
2021-03-02 09:11:08 -0500

    Modificando Usuario.h

--- Usuario.h
+++ Usuario.h
@@ -14,3 +14,9 @@ class Usuario{
 		void ModificarUser();		
 	
 };
+void Usuario::MostrarUser(){
+	cout<<"1. Nombre: "<<NombreUser<<endl;
+	cout<<"2. RUC: "<<Ruc<<endl;
+	cout<<"3. DNI: "<<DNI<<endl;
+	cout<<"4. Celular: "<<Celular<<endl;
+}
