127782e8355fa5992fef567d9aa787d587308ca3
JosePizarroRabanal <78873334+JosePizarroRabanal@users.noreply.github.com>
2021-03-02 09:42:35 -0500

    Completando clase Descripcion

--- Descripcion.h
+++ Descripcion.h
@@ -1,10 +1,41 @@
-#ifndef DESCRIPCION_H
-#define DESCRIPCION_H
+#include <iostream>
+#include<conio.h>
+using namespace std;
 
-class Descripcion
-{
+class Descripcion{
 	public:
-	protected:
+		float SubTotal;
+		int cantidad;
+		Producto* Po;
+	public:
+		void producto(Producto *);
+		void ProductoDatos();
+		int getCantidad();
+		void CrearFactura();
+	
 };
+int Descripcion::getCantidad(){
+	return cantidad;
+}
+void Descripcion::producto(Producto *Pro ){
+	Po=Pro;
+	cout<<"Producto elegido correctamente"<<endl;
+}
+void Descripcion::ProductoDatos(){
+	SubTotal=Po->GetPrecio() *cantidad;
+	cout<<left;
+	cout<<setw(22)<<Po->GetNombre();
+	cout<<setw(7)<<Po->GetPrecio();
+	cout<<setw(10)<<cantidad;
+	cout<<setw(11)<<SubTotal;
+	cout<<endl;
+	
+}
+//int Descripcion::getnombre(Producto* po){
+//	return po->GetNombre();
+//}
+void Descripcion::CrearFactura(){
+	
+	
+}
 
-#endif
