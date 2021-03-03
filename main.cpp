8c794f78318409f700c127e7cf392c317cc7bea9
JosePizarroRabanal <78873334+JosePizarroRabanal@users.noreply.github.com>
2021-03-02 19:01:37 -0500

    swtich de main

--- main.cpp
+++ main.cpp
@@ -69,13 +69,83 @@ int main(){
 
 	switch(cen){
 		case 1:
-
+	system("cls");
+			user.MostrarUser();
+			cout<<"------------------------------------------------------------------------"<<endl;	
+			user.ModificarUser();
 		break;
 		case 2:
-
+				system("cls");
+					cout<<left;
+					cout<<setw(4)<<"N";
+					cout<<setw(8)<<"Cod.";
+					cout<<setw(10)<<"Stock";
+					cout<<setw(22)<<"Nombre";
+					cout<<setw(7)<<"Precio";
+					cout<<setw(20)<<"Detalles";
+					cout<<endl;
+					cout<<"------------------------------------------------------------------------"<<endl;	
+		for(int k=1;k<i;k++){
+		cout<<left<<setw(3)<<k;					
+		Pro[k].MostrarProducto();
+		cout<<endl;
+	}
+		cout<<"------------------------------------------------------------------------"<<endl;		
+	cout<<"Que Producto desea : ";
+	cin>>op;
+	if(op>i-1){
+		system("cls");
+		cout<<"Error el producto que desea no existe , vuelva a selecionar "<<endl;
+		system("pause");
+	}
+	else{
+			c=1;
+		do{
+		
+		system("cls");
+					cout<<left;
+					cout<<setw(8)<<"Cod.";
+					cout<<setw(10)<<"Stock";
+					cout<<setw(22)<<"Nombre";
+					cout<<setw(7)<<"Precio";
+					cout<<setw(20)<<"Detalles";
+					cout<<endl;
+					cout<<"------------------------------------------------------------------------"<<endl;						
+		Pro[op].MostrarProducto();
+		cout<<endl;
+		cout<<"\t Cantidad del producto : ";
+		cin>>Des[h].cantidad;
+		if((Des[h].cantidad==3) || (Des[h].cantidad==6) || (Des[h].cantidad==12) || (Des[h].cantidad==24) ){
+			c=0;
+			Des[h].producto(&Pro[op]);
+			Pro[op].ActualizarCantidad(Des[h].cantidad );
+			h++;
+		}
+			}while(c==1);
 		break;
 		case 3:
+				if(i!=0){
+			system("cls");
+					cout<<left;
+					cout<<setw(4)<<"N ";
 
+					cout<<setw(22)<<"Nombre";
+					cout<<setw(7)<<"Precio";
+					cout<<setw(10)<<"Cantidad";
+					cout<<setw(11)<<"SubTotal";
+					cout<<endl;
+					cout<<"------------------------------------------------------------------------"<<endl;	
+					for(int r=1;r<h;r++){
+						cout<<left<<setw(4)<<r;	
+					Des[r].ProductoDatos();		
+					}
+					system("pause");
+			
+			
+		}
+		else{
+			cout<<"Aun no cuenta con productos en la factura "<<endl;
+		}
 		break;
 		case 4:
 		
